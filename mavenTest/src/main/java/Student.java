e04e8c8b7ee8b079e5bbd9a35e9b9a34aab5e868
gaoxiaofei90 <1821464821@qq.com>
2020-03-06 10:48:12 +0800

    添加学生实体类

---
+++ mavenTest/src/main/java/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+
+    private Integer age ;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
