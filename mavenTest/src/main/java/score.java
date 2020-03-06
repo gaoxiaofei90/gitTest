f162b5a7052da8a3aae87a9746376f9baf4268a6
gaoxiaofei90 <1821464821@qq.com>
2020-03-06 11:31:36 +0800

    添加分数实体类

---
+++ mavenTest/src/main/java/score.java
@@ -0,0 +1,11 @@
+public class score {
+    private float scoreId;
+
+    public float getScoreId() {
+        return scoreId;
+    }
+
+    public void setScoreId(float scoreId) {
+        this.scoreId = scoreId;
+    }
+}
